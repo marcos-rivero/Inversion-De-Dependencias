0e878cbbce225a58936308784fcf9d7e4ba9f03a
Marcos Rivero <marcosrivero_d4@hotmail.com>
2024-09-10 17:33:32 -0300

    first commit

---
+++ AlmacenamientoSQL.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InversionDependencia
+{
+    public class AlmacenamientoSQL : IAlmacenamiento
+    {
+        private readonly DBRepositorio _dBRepositorio;
+        public AlmacenamientoSQL(DBRepositorio dBRepositorio)
+        {
+            _dBRepositorio = dBRepositorio;
+        }
+
+        public void Guardar(string titulo, string contenido)
+        {
+            _dBRepositorio.Guardar(titulo, contenido);
+        }
+       
+        public string Leer(string titulo)
+        {
+            return _dBRepositorio.Leer(titulo);
+        }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using InversionDependencia;
+using Microsoft.Extensions.DependencyInjection;
+
+Console.WriteLine("Hello, World!");
+var serviceProvider = new ServiceCollection()
+    .AddScoped<IAlmacenamiento, AlmacenamientoSQL>()
+    .AddScoped<ILogging, Logging>()
+    .AddScoped<IArchivo, Archivo>()
+    .AddScoped<DBRepositorio>()
+    .AddScoped<Articulo>()
+    .AddScoped<Cache>().BuildServiceProvider();
+
+var articulo = serviceProvider.GetService<Articulo>();
+articulo.ConsultarArticulo("test");
